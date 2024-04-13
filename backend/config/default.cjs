7676608171cdf7d1bc1257d0a9d096200d68b51b
Aq202 <diegoguatedb2002@gmail.com>
2024-04-12 18:29:22 -0600

    feat: credenciales neo4j agregadas

--- backend/config/default.cjs
+++ backend/config/default.cjs
@@ -8,4 +8,7 @@ module.exports = {
 		"mongodb+srv://diego:123@clusteruvg.aryjpty.mongodb.net/?retryWrites=true&w=majority&appName=clusterUVG",
 	jwtKey: "a8b08ebce1d6ad7f0eb8f0815385ab5ad9af9d7b02f8c744abaa8d8f52541d7c",
 	sendErrorObj: true,
+	neo4j_uri: "neo4j+s://9a670284.databases.neo4j.io",
+	neo4j_user: "neo4j",
+	neo4j_password: "q341vIR2TmgIvedPcx_pl3m5Y9B7ab-tTiBmY7cIEek"
 };
