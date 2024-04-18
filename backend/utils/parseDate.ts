66fdf2f811a10ca32b91c01d711f4da44cadb918
Aq202 <diegoguatedb2002@gmail.com>
2024-04-17 20:08:06 -0600

    fix: parse date corregido

--- backend/utils/parseDate.ts
+++ backend/utils/parseDate.ts
@@ -1,8 +1,9 @@
 const parseDate = (date:Date) => {
 
-  const year: number = date.getFullYear();
-  let month: string | number = date.getMonth() + 1;
-  let day: string | number = date.getDate();
+  const d = new Date(date);
+  const year: number = d.getFullYear();
+  let month: string | number = d.getMonth() + 1;
+  let day: string | number = d.getDate();
 
   month = month < 10 ? '0' + month : month;
   day = day < 10 ? '0' + day : day;
